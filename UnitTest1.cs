facc3af159880bbc99cd5207b15ac06124565865
HeshyaVk <vkheshya@gmail.com>
2026-02-16 15:24:15 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace PlaywrightDemo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+       //Playwright
+       using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 500
+        });
+
+        //page
+        var page = await browser.NewPageAsync();
+    
+        await page.GotoAsync("http://www.eaapp.somee.com/");
+        await page.ClickAsync("text=login");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAApp.jpg"
+        });
+        await page.FillAsync("#UserName", "admin");
+        await page.FillAsync("#Password", "password");
+        await page.ClickAsync("text=log in");
+        var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
+        Assert.That(isExist, Is.True);
+
+    }
+}
+
+
