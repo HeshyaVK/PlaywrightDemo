d6432e09cd47bcc2a7d9be982853ff461dabdaa6
HeshyaVk <vkheshya@gmail.com>
2026-02-16 15:29:00 +0530

    nunituncommanded

--- NUnitPlaywright.cs
+++ NUnitPlaywright.cs
@@ -1,30 +1,30 @@
-﻿//using Microsoft.Playwright;
-//using Microsoft.Playwright.NUnit;
-//using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System.Threading.Tasks;
 
-//namespace PlaywrightDemo;
+namespace PlaywrightDemo;
 
-//public class NUnitPlaywright : PageTest
-//{
-//    [SetUp]
-//    public async Task Setup()
-//    {
-//        await Page.GotoAsync("http://www.eaapp.somee.com/");
-//    }
+public class NUnitPlaywright : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://www.eaapp.somee.com/");
+    }
 
-//    [Test]
-//    public async Task Test1()
-//    {
-   
-//        await Page.ClickAsync("text=login");
-//        await Page.FillAsync("#UserName", "admin");
-//        await Page.FillAsync("#Password", "password");
-//        await Page.ClickAsync("text=log in");
-//        await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
-        
-        
+    [Test]
+    public async Task Test1()
+    {
 
-//    }
-//}
+        await Page.ClickAsync("text=login");
+        await Page.FillAsync("#UserName", "admin");
+        await Page.FillAsync("#Password", "password");
+        await Page.ClickAsync("text=log in");
+        await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
+
+
+
+    }
+}
 
 
